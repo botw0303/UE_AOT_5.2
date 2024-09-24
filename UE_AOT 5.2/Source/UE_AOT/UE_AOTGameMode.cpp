d14905c1e931fa6b79a5528f4377008312968d0c
botw0303 <zeldabotw0303@gmail.com>
2024-09-25 00:38:53 +0900

    거인 자동 생성

--- UE_AOT 5.2/Source/UE_AOT/UE_AOTGameMode.cpp
+++ UE_AOT 5.2/Source/UE_AOT/UE_AOTGameMode.cpp
@@ -3,13 +3,83 @@
 #include "UE_AOTGameMode.h"
 #include "UE_AOTCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include <Kismet/GameplayStatics.h>
+#include <NavigationSystem.h>
+#include <Collision/CollisionDefine.h>
 
 AUE_AOTGameMode::AUE_AOTGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
+
+}
+
+void AUE_AOTGameMode::BeginPlay()
+{
+	// 5마리 미리 스폰
+	for (int i = 0; i < 5; ++i)
 	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
+		SpawnGiant();
+	}
+
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &AUE_AOTGameMode::CheckTotalGiantCount, 60.0f, true);
+}
+
+void AUE_AOTGameMode::CheckTotalGiantCount()
+{
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AGiantCharacter::StaticClass(), Giants);
+	if (Giants.Num() >= MaxGiantCnt)
+	{
+		return;
+	}
+
+	SpawnGiant();
+}
+
+void AUE_AOTGameMode::SpawnGiant()
+{
+	if (GiantActor)
+	{
+		UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetNavigationSystem(GetWorld());
+		if (NavSystem)
+		{
+			FNavLocation SpawnPos;
+
+			FVector SpawnLocation = FVector(0, 0, 0);
+			FRotator SpawnRotation = FRotator::ZeroRotator;
+
+			AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(GiantActor, SpawnLocation, SpawnRotation);
+
+			if (SpawnedActor)
+			{
+				float RandomScaleValue = FMath::FRandRange(10.0f, 15.0f);
+
+				SpawnedActor->SetActorRelativeScale3D(FVector(RandomScaleValue));
+
+				SpawnedActor->SetActorLocation(DefaultSpawnLocations[LocationIdx % DefaultSpawnLocations.Num()]);
+				LocationIdx++;
+			}
+		}
 	}
 }
+
+bool AUE_AOTGameMode::IsLocationValid(const FVector& Location, float Radius, AActor* IgnoreActor)
+{
+	TArray<FOverlapResult> OverlapResults;
+	FCollisionQueryParams CollisionQueryParam(SCENE_QUERY_STAT(Detect), false, this);
+
+	//CollisionQueryParam.AddIgnoredActor(IgnoreActor);
+
+	bool bHasOverlaps = GetWorld()->OverlapMultiByChannel(
+		OverlapResults,
+		Location,
+		FQuat::Identity,
+		GAMETRACECHANNEL,
+		FCollisionShape::MakeSphere(Radius),
+		CollisionQueryParam
+	);
+
+	return !bHasOverlaps || OverlapResults.Num() == 0;
+}
+
+
+
+
--- UE_AOT 5.2/Source/UE_AOT/UE_AOTGameMode.h
+++ UE_AOT 5.2/Source/UE_AOT/UE_AOTGameMode.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include <Giant/GiantCharacter.h>
 #include "UE_AOTGameMode.generated.h"
 
 UCLASS(minimalapi)
@@ -13,6 +14,26 @@ class AUE_AOTGameMode : public AGameModeBase
 
 public:
 	AUE_AOTGameMode();
+
+protected:
+	virtual void BeginPlay() override;
+
+	void CheckTotalGiantCount();
+	void SpawnGiant();
+	bool IsLocationValid(const FVector& Location, float Radius, AActor* IgnoreActor);
+
+protected:
+	FTimerHandle TimerHandle;
+	UPROPERTY(EditDefaultsOnly, Category = "SpawnGiant")
+	TSubclassOf<AActor> GiantActor;
+	TArray<AActor*> Giants;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "SpawnGiant")
+	int MaxGiantCnt;
+	bool bIsGiantCntMax;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "SpawnGiant")
+	TArray<FVector> DefaultSpawnLocations;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "SpawnGiant")
+	int LocationIdx = 0;
 };
 
 
